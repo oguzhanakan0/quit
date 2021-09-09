4467f112ef15c37daabcc244ad6df09ac3c6ff7a
Oguzhan Akan <oakan13@ku.edu.tr>
2021-09-09 14:09:56 -0700

    added first joined to signin

--- src/user/views.py
+++ src/user/views.py
@@ -5,6 +5,7 @@ from firebase_admin import auth
 from django.http import JsonResponse
 from .models import User
 from django.forms.models import model_to_dict
+import datetime
 
 
 # cred = credentials.Certificate("C:\\Users\\mulku\\Desktop\\iquit-507f7-firebase-adminsdk-go447-bc8b2413f2.json")
@@ -16,28 +17,34 @@ default_app = firebase_admin.initialize_app(cred)
 
 def sign_in(request):
     print(request.body)  # Debug
-    try:
-        d = json.loads(request.body)
-        id_token = d["token"]
-        _user = auth.verify_id_token(id_token)
-        print(f"_user: {_user}")  # Debug
-        _nuser = {
-            "uid":_user["uid"],
-            "first_name":" ".join(_user["name"].split(" ")[:-1]),
-            "last_name": _user["name"].split(" ")[-1],
-            "auth_source": _user["firebase"]["sign_in_provider"],
-            "email": _user["email"]
-        }
-        user, created = User.objects.get_or_create(_nuser)
-        print(f"created?: {created}")  # Debug
-        return JsonResponse({
-            "success": True,
-            "user": model_to_dict(user,fields=["first_name","last_name","is_info_complete","email","username"])
-        })
-    except:
-        return JsonResponse({
-            "success": False
-        })
+    # try:
+    d = json.loads(request.body)
+    id_token = d["token"]
+    _user = auth.verify_id_token(id_token)
+    print(f"_user: {_user}")  # Debug
+    _nuser = {
+        "uid":_user["uid"],
+        "first_name":" ".join(_user["name"].split(" ")[:-1]),
+        "last_name": _user["name"].split(" ")[-1],
+        "auth_source": _user["firebase"]["sign_in_provider"],
+        "email": _user["email"]
+    }
+    user, created = User.objects.get_or_create(_nuser)
+    print(f"created?: {created}")  # Debug
+    if created:
+        print("setting first_joined to")
+        user.first_joined = datetime.date.today()
+        user.save()
+    else:
+        print(f"not created: {user.email}, {user.uid}, {user.id}")
+    return JsonResponse({
+        "success": True,
+        "user": model_to_dict(user,fields=["first_name","last_name","is_info_complete","email","username"])
+    })
+    # except:
+    #     return JsonResponse({
+    #         "success": False
+    #     })
 
 
 def update_user(request):
